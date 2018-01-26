0f5a9a7bfd409b915e8a6e8e7010a9d3610920c7
jens87alpen <jenshoffmann20@gmail.com>
2018-01-26 12:37:52 -0800

    first commit

---
+++ Node/keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+
+module.exports = {
+  consumer_key: 'ja3HvR7iPLvU5rzE4TPSAfnfX',
+  consumer_secret: 'RydIwsAUsWInh0UEkXw9no1vDLAuzL4RGtBVI3Z3dGcZxBsUGA',
+  access_token_key: '956645282095144960-GCrHs2rUfGQfgvcjlqKN05qgiKlbt0W',
+  access_token_secret: 't3lT7ovSsQOOrK4CLDiUnqMe1qAuv9t4NU4pJmKUQrDgC'
+};
+
---
+++ Node/spotify.js
@@ -0,0 +1,7 @@
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: 'e0244453599d4ac8a1ae1f7f9a721a31',
+  secret: '7e050448556541b7aadfb98fd9a300fa'
+});
+ 
